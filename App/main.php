063352ea76318db34b1ffd9ed4dbbf7aaa40d225
dslosky <danielslosky@hotmail.com>
2015-04-10 03:35:22 +0000

    Header looks good!

---
+++ App/main.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * To change this template use Tools | Templates.
+ */
+?>
+
+<HTML>
+    <head>
+        <title>RCTL</title>
+        
+        <link href="/CSS/bootstrap.css" rel="stylesheet">
+        <link href="/CSS/jquery-ui.css" rel="stylesheet">
+        <link href="/CSS/main.css" rel="stylesheet">
+        
+    </head>
+    <body>
+        
+        <div clas="header">
+            <div class="row">
+                <div class="col-md-1 logoContainer">
+                    <div class="logo">
+                        
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="nav">
+                        <div class="container">
+                            <ul class="pull-left">
+                                <li class="logo">LOGO</li>
+                                <li><a href="#">Calendar</a></li>
+                                <li><a href="#">Logs</a></li>
+                                <li><a href="#">RCTM Page</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="/JS/jquery-ui.js"></script>
+        <script src="/JS/bootstrap.js"></script>
+        <script src="/JS/App.js"></script>
+    </body>
+</HTML>

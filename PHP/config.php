9d22c2ad6f7536e9e64393219ffe14317e31ef08
Rick Jamil <shariq1989@gmail.com>
2015-05-31 18:42:54 -0600

    moved credentials

--- PHP/config.php
+++ PHP/config.php
@@ -1,11 +1,14 @@
 <?php
+
+#import credentials
+include_once 'creds/cred-config.php';
+
 ########## MySql details #############
-$username =     "climbingmanager"; //mysql username
-$password =     "pass";            //mysql password
-$hostname =     "localhost";       //hostname
-$databasename = "allTables";       //databasename
+$username =     USER;       //mysql username
+$password =     PASSWORD;   //mysql password
+$hostname =     HOST;       //hostname
+$databasename = DATABASE;   //databasename
 
 //connect to database
 $conn = new mysqli($hostname, $username, $password, $databasename);
-
 ?>
---
+++ PHP/creds/cred-config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * MySQL database login details
+ */
+define("HOST", "localhost");
+define("USER", "climbingmanager");
+define("PASSWORD", "pass");
+define("DATABASE", "allTables");
+?>

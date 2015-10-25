519e84648457eed76da70474f730a647310ac119
Daniel Slosky <danielslosky@gmail.com>
2015-10-25 20:12:33 +0000

    Fixed mysql access issue on AWS

--- PHP/config.php
+++ PHP/config.php
@@ -3,7 +3,7 @@
 $username =     "climbingmanager"; //mysql username
 $password =     "pass";            //mysql password
 $hostname =     "localhost";       //hostname
-$databasename = "allTables";       //databasename
+$databasename = "alltables";       //databasename
 
 //connect to database
 $conn = new mysqli($hostname, $username, $password, $databasename);
